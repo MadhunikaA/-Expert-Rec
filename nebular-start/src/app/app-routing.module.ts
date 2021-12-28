10b474101668fc30bde5ce8c6350b07fbb0ce6ad
madhunika <madhuakula6778@gmail.com>
2021-12-28 22:20:59 +0530

    auth-app

--- nebular-start/src/app/app-routing.module.ts
+++ nebular-start/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
-import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuardService } from './auth-guard.service';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuardService] }, // only accessible if authorised
+  { path: '**', component: LoginComponent }
+];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+export const AppRoutingModule = RouterModule.forRoot(routes)
--- nebular-start/src/app/app.module.ts
+++ nebular-start/src/app/app.module.ts
@@ -1,25 +1,65 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
+
+//nebular Components
+import { NbThemeModule, NbLayoutModule, NbCardModule, NbButtonModule, NbInputModule, NbFormFieldModule, NbButtonGroupModule, NbAlertModule } from '@nebular/theme';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { NbIconModule } from '@nebular/theme';
+
+//feature Components
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { AppComponent } from './app.component';
+
+//auth-gurad-services
+import { AuthGuardService } from './auth-guard.service';
+import { GoogleLoginProvider, SocialLoginModule } from 'angularx-social-login';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot([
+      { path: 'login', component: LoginComponent },
+      { path: 'profile', component: ProfileComponent, canActivate: [AuthGuardService] }, // only accessible if authorised
+      { path: '**', component: LoginComponent }
+    ]),
+    BrowserAnimationsModule,
+    SocialLoginModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     NbThemeModule.forRoot({ name: 'dark' }),
     NbLayoutModule,
-    NbEvaIconsModule
+    NbEvaIconsModule,
+    NbCardModule,
+    NbButtonModule,
+    NbIconModule,
+    NbInputModule,
+    NbFormFieldModule,
+    NbButtonGroupModule,
+    NbAlertModule
   ],
-  providers: [],
+  providers: [{
+    provide: 'SocialAuthServiceConfig',
+    useValue: {
+      autoLogin: true, //keeps the user signed in
+      providers: [
+        {
+          id: GoogleLoginProvider.PROVIDER_ID,
+          provider: new GoogleLoginProvider('884713958485-5eh255742r5u6k1ii7qlbsuc6o1q00pj.apps.googleusercontent.com') // your client id
+        }
+      ]
+    }
+  },
+  AuthGuardService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
